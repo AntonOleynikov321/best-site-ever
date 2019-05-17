4c513dd24c9f9779f161e014168a947155bb3b77
DmitriyLishikov <pain14141919@gmail.com>
2019-05-17 17:16:09 +0300

    form group

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="row">
@@ -13,5 +12,24 @@
             </div>
         </div>
     </div>
+</div>
+<div class="panel-body">
+    <!-- Форма новой задачи -->
+    <form method='post' action="{{ route('/home') }}">
+        {{ csrf_field() }}
+        {{method_field('put')}}                                     
+        <div class="form-group">
+            <label for="task" class="col-sm-3 control_label"></label>
+            <div class="col-sm-6">
+                <input type="text" name="name" id="task-name" class="form-control" value="" >
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="col-sm-offset-3 col-sm-6">
+                <button type="submit" clas="btn btn-default bg-succes">
+                    <i class="fa fa-plus"></i>Добавить группу
+                </button>
+            </div>
+    </form>
 </div>
 @endsection
