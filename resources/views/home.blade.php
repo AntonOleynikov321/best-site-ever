731a8760bf86331556256d2234e940b423ac0865
DmitriyLishikov <pain14141919@gmail.com>
2019-05-18 00:56:47 +0300

    insert bd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,22 +14,24 @@
     </div>
 </div>
 <div class="panel-body">
+ <!-- Отображение ошибок проверки ввода -->
+    @include('common.errors')
     <!-- Форма новой группы -->
-    <form method='post' action="{{ route('') }}">
+    <form action="{{route('home.store')}}" method="POST" class="form-horizontal">
         {{ csrf_field() }}
-        {{method_field('get')}}                                     
-        <div class="form-group">
-            <label for="task" class="col-sm-3 control_label"></label>
+        <div class="form-group">         
             <div class="col-sm-6">
-                <input type="text" name="name" id="task-name" class="form-control" value="" >
+                <input type="text" name="name" id="group-name" class="form-control">
             </div>
         </div>
+        <!-- Кнопка добавления группы -->
         <div class="form-group">
             <div class="col-sm-offset-3 col-sm-6">
-                <button type="submit" clas="btn btn-default bg-succes">
+                <button type="submit" class="btn btn-default bg-success">
                     <i class="fa fa-plus"></i> Создать группу
                 </button>
             </div>
+        </div>
     </form>
 </div>
 @endsection
