27e94b68fdf1efb75504261f971c7b445055fc3e
smirnovDm <smirnov.dm31@gmail.com>
2019-05-18 13:20:59 +0300

    delete Groups.php

--- app/Group.php
+++ app/Group.php
@@ -4,11 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Groups extends Model
-{
-      protected $fillable = ['name'];
-      
-       public function user() {
+class Group extends Model {
+
+    protected $fillable = ['name'];
+
+    public function user() {
         return $this->belongsTo(User::class);
     }
+
 }
