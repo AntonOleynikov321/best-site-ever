ad7c3f5f93a7ce62ff5e606a796124da806d2e01
master <qewrqw@dwfds.com>
2019-05-21 20:15:44 +0300

    create groupscontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,9 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
-use App\Group;
-use App\User;
 
 class HomeController extends Controller {
 
@@ -17,47 +14,6 @@ class HomeController extends Controller {
      */
     public function __construct() {
         $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request, Group $groups) {
-
-//        $user = $request->user();
-//        $teach_groups = $user->teach_groups();
-//        foreach ($teach_groups as $group) {
-//            echo $group->name;
-//        }
-//       $
-        $groups = Group::All();
-        return view('groups.index', [
-            'groups' => $groups,
-        ]);
-    }
-
-    public function addGroup(Group $groups) {
-        return view('groups.create', [
-            'groups' => $groups,
-        ]);
-    }
-
-    public function createGroup(Request $request, Group $groups) {
-        $this->validate($request, [
-            'name' => 'required|max:100',
-        ]);
-        $groups = new Group();
-        $groups->name = $request->name;
-        $groups->save();
-        return redirect(route('home_index'));
-    }
-
-    public function deleteGroup( Group $groups) {
-//        $this->authorise('destroy', $groups);
-        $groups->delete();
-        return redirect(route('home_index'));
     }
 
 }
