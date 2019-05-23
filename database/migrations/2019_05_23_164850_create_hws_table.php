ee5a0b692f5ba032e91d14d5f5de7344df5667cd
introqt <introtheintro@gmail.com>
2019-05-23 19:55:44 +0300

    hws_table

---
+++ database/migrations/2019_05_23_164850_create_hws_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHwsTable extends Migration
+{
+    public function up() {
+        Schema::create('hws_table', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('text');
+            $table->string('file_name');
+            $table->data('finish');
+            $table->integer('user_id')->unsigned();
+            $table->integer('group_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('group_id')->references('id')->on('groups');
+        });
+    }
+
+   
+    public function down() {
+        Schema::drop('hws_table');
+    }
+}
