e0fff1f9bfeb670bb8b24e719e6e050a2bde8806
Vasyl <admin@gmail.com>
2019-05-23 20:57:14 +0300

    weqdsc

--- resources/views/add_student/add.blade.php
+++ resources/views/add_student/add.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 
-<form action="{{route('invite_student', 2)}}" method="POST">
+<form action="{{route('invite_student', 1)}}" method="POST">
+    {{csrf_field()}}
     <input type="text" name='login' placeholder="enter login"/>
     <button type="submit" class="btn btn-default"> 
 	<i class="fa fa-plus"></i> 
