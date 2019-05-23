7ae699422900aed5579f46bf7c281d3495bde9a3
introqt <introtheintro@gmail.com>
2019-05-23 19:23:39 +0300

    pupa lupa

--- app/Http/Controllers/GroupsController.php
+++ app/Http/Controllers/GroupsController.php
@@ -5,14 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 
-class GroupController extends Controller {
+class GroupsController extends Controller {
 
     public function __construct() {
         $this->middleware('auth');
     }
 
-    public function index() {
-        return view('group.index');
+    public function show() {
+        return view('groups.index');
     }
 
 }
